9911baa597ff512b693c0ada44e9b88449cff0bc
Secu-Design <mailto.devel@secu-design.com>
2025-03-22 17:48:40 +0100

    Optimize MSVC compiler flags

--- CMake/toolchain-general.cmake
+++ CMake/toolchain-general.cmake
@@ -14,15 +14,15 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 
    # Compiler flags
-   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /permissive- /W3 /GS /Zc:wchar_t /Gm- /sdl /Zc:inline /fp:precise /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /FC /EHsc /nologo /diagnostics:column")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /permissive- /Yu\"pch.h\" /ifcOutput /W3 /GS /Zc:wchar_t /Gm- /sdl /Zc:inline /fp:precise /D \"_UNICODE\" /D \"UNICODE\" /errorReport:prompt /WX- /Zc:forScope /Gd /FC /EHsc /nologo /diagnostics:column")
    # Compiler flags - DEBUG
-   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /D "DEBUG" /JMC /ZI /Od /RTC1 /MDd")
+   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /D \"DEBUG\" /JMC /ZI /Od /RTC1 /MDd")
    # Compiler flags - RELEASE
-   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /D "NDEBUG" /GL /Zi /MD /O2")
+   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /D \"NDEBUG\" /GL /Zi /MD /O2")
 
    # Linker flags - STATIC LIBS
-   set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /D "_LIB"")
+   set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /D \"_LIB\"")
    # Linker flags - SHARED LIBS
-   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /D "DLL1_EXPORTS" /D "_WINDOWS" /D "_USRDLL" /D "_WINDLL"")
+   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /D \"DLL1_EXPORTS\" /D \"_WINDOWS\" /D \"_USRDLL\" /D \"_WINDLL\"")
 
 endif ()
