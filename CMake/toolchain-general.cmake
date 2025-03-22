58802517a8245818ae593557546f985de3cb217d
Secu-Design <mailto.devel@secu-design.com>
2025-03-22 16:17:15 +0100

    Optimize compiler flags for MSVC

--- CMake/toolchain-general.cmake
+++ CMake/toolchain-general.cmake
@@ -4,20 +4,25 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 
-   # General flags
+   # Compiler flags
    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror=unused-result -fPIC")
-   # Debug flags
+   # Compiler flags - DEBUG
    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g")
-   # Release flags
+   # Compiler flags - RELEASE
    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2")
 
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 
-   # General flags
-   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4 /permissive- /EHsc /sdl")
-   # Debug flags
-   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /DEBUG /Zi /Od /RTC1")
-   # Release flags
-   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /DNDEBUG /O2")
+   # Compiler flags
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /permissive- /W3 /GS /Zc:wchar_t /Gm- /sdl /Zc:inline /fp:precise /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /FC /EHsc /nologo /diagnostics:column")
+   # Compiler flags - DEBUG
+   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /D "DEBUG" /JMC /ZI /Od /RTC1 /MDd")
+   # Compiler flags - RELEASE
+   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /D "NDEBUG" /GL /Zi /MD /O2")
+
+   # Linker flags - STATIC LIBS
+   set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /D "_LIB"")
+   # Linker flags - SHARED LIBS
+   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /D "DLL1_EXPORTS" /D "_WINDOWS" /D "_USRDLL" /D "_WINDLL"")
 
 endif ()
