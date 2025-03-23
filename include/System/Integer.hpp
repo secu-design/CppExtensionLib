98a274860220abeaf5afaf443ecb1bb7511ed9db
Secu-Design <mailto.devel@secu-design.com>
2025-03-23 19:25:38 +0100

    - Add doxygen comments

--- include/System/Integer.hpp
+++ include/System/Integer.hpp
@@ -16,6 +16,17 @@ namespace ext::System
         static std::string ToString(T number);
     };
 
+    /**
+     * @brief Converts an integer to a two-digit hexadecimal string.
+     *
+     * This function converts the given integer number to its two-digit hexadecimal representation.
+     * The type of the number must be an integral type. The resulting hexadecimal string will always
+     * be at least two characters long, padded with leading zeros if necessary.
+     *
+     * @tparam T The type of the number, which must be an integral type.
+     * @param number The integer number to convert to a two-digit hexadecimal string.
+     * @return std::string The two-digit hexadecimal representation of the given number.
+     */
     template<typename T>
     inline std::string Integer::ToHex(const T number)
     {
@@ -42,6 +53,16 @@ namespace ext::System
         return hexStream.str();
     }
 
+    /**
+     * @brief Converts an integer to a string.
+     *
+     * This function converts the given integer number to a string representation. The type of the
+     * number must be an integral type. The resulting string will contain the number as a string.
+     *
+     * @tparam T The type of the number, which must be an integral type.
+     * @param number The integer number to convert to a string.
+     * @return std::string The string representation of the given number.
+     */
     template<typename T>
     inline std::string Integer::ToString(const T number)
     {
