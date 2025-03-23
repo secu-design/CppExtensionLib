425cdabecbb16ab0618a4deef230885fea6bf8ab
Secu-Design <mailto.devel@secu-design.com>
2025-03-23 19:57:21 +0100

    Refactoring

--- test/Test_Integer_ToHex.cpp
+++ test/Test_Integer_ToHex.cpp
@@ -9,84 +9,84 @@ using namespace ext::System;
 
 // Signed integers - Max values
 
-TEST(Integer_ToHex, SignedChar_MaxValue)
+TEST(Integer_ToHex, Char_MaxValue)
 {
     constexpr signed char value{ std::numeric_limits<signed char>::max() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "7f");
 }
 
-TEST(Integer_ToHex, SignedShort_MaxValue)
+TEST(Integer_ToHex, Short_MaxValue)
 {
-    constexpr signed short value{ std::numeric_limits<signed short>::max() };
+    constexpr short value{ std::numeric_limits<short>::max() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "7fff");
 }
 
-TEST(Integer_ToHex, SignedInt_MaxValue)
+TEST(Integer_ToHex, Int_MaxValue)
 {
-    constexpr signed int value{ std::numeric_limits<signed int>::max() };
+    constexpr int value{ std::numeric_limits<int>::max() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "7fffffff");
 }
 
 #ifdef _MSC_VER
-    TEST(Integer_ToHex, SignedLong_MaxValue)
+    TEST(Integer_ToHex, Long_MaxValue)
     {
-        constexpr signed long value{ std::numeric_limits<signed long>::max() };
+        constexpr long value{ std::numeric_limits<long>::max() };
         const std::string hexNumber{ Integer::ToHex(value) };
         EXPECT_EQ(hexNumber, "7fffffff");
     }
 #elif defined(__GNUC__)
-    TEST(Integer_ToHex, SignedLong_MaxValue)
+    TEST(Integer_ToHex, Long_MaxValue)
     {
-        constexpr signed long value{ std::numeric_limits<signed long>::max() };
+        constexpr long value{ std::numeric_limits<long>::max() };
         const std::string hexNumber{ Integer::ToHex(value) };
         EXPECT_EQ(hexNumber, "7fffffffffffffff");
     }
 #endif
 
-TEST(Integer_ToHex, SignedLongLong_MaxValue)
+TEST(Integer_ToHex, LongLong_MaxValue)
 {
-    constexpr signed long long value{ std::numeric_limits<signed long long>::max() };
+    constexpr long long value{ std::numeric_limits<long long>::max() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "7fffffffffffffff");
 }
 
 // Signed integers -> Min values
 
-TEST(Integer_ToHex, SignedChar_MinValue)
+TEST(Integer_ToHex, Char_MinValue)
 {
     constexpr signed char value{ std::numeric_limits<signed char>::min() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "80");
 }
 
-TEST(Integer_ToHex, SignedShort_MinValue)
+TEST(Integer_ToHex, Short_MinValue)
 {
-    constexpr signed short value{ std::numeric_limits<signed short>::min() };
+    constexpr short value{ std::numeric_limits<short>::min() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "8000");
 }
 
-TEST(Integer_ToHex, SignedInt_MinValue)
+TEST(Integer_ToHex, Int_MinValue)
 {
-    constexpr signed int value{ std::numeric_limits<signed int>::min() };
+    constexpr int value{ std::numeric_limits<int>::min() };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "80000000");
 }
 
 #ifdef _MSC_VER
-TEST(Integer_ToHex, SignedLong_MinValue)
+TEST(Integer_ToHex, Long_MinValue)
     {
-        constexpr signed long value{ std::numeric_limits<signed long>::min() };
+        constexpr long value{ std::numeric_limits<long>::min() };
         const std::string hexNumber{ Integer::ToHex(value) };
         EXPECT_EQ(hexNumber, "80000000");
     }
 #elif defined(__GNUC__)
-    TEST(Integer_ToHex, SignedLong_MinValue)
+    TEST(Integer_ToHex, Long_MinValue)
     {
-        constexpr signed long value{ std::numeric_limits<signed long>::min() };
+        constexpr long value{ std::numeric_limits<long>::min() };
         const std::string hexNumber{ Integer::ToHex(value) };
         EXPECT_EQ(hexNumber, "8000000000000000");
     }
@@ -139,16 +139,16 @@ TEST(Integer_ToHex, UnsignedLongLong_MaxValue) {
 
 // Other tests
 
-TEST(Integer_ToHex, SignedIntZero)
+TEST(Integer_ToHex, Zero)
 {
-    constexpr signed int value{};
+    constexpr long value{};
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "00");
 }
 
-TEST(Integer_ToHex, UnsignedIntZero)
+TEST(Integer_ToHex, NegativeZero)
 {
-    constexpr unsigned int value{};
+    constexpr long value{ -0 };
     const std::string hexNumber{ Integer::ToHex(value) };
     EXPECT_EQ(hexNumber, "00");
 }
